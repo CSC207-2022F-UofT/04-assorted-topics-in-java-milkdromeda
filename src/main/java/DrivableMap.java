e107c94f815ed9de2b8611d1438fd03a893c45d0
milkdromeda <jchen9827@gmail.com>
2022-10-09 23:44:35 -0400

    mesg

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -27,6 +27,15 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
+    public boolean addDrivable(String id, Drivable item) {
+        if (drivable_map.get(id) != null) {
+            drivable_map.put(id, item);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
 
 
@@ -37,6 +46,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
+    public boolean hasFasterThan(int speed) {
+        for (Drivable value : drivable_map.values()) {
+            if (value.getMaxSpeed() >= speed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,6 +63,16 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
+    public List<Tradable> getTradable() {
+        List<Tradable> lst = new ArrayList<>();
+        for (Drivable value : drivable_map.values()) {
+            if (value instanceof Tradable) {
+                lst.add((Tradable) value);
+            }
+        }
+        return lst;
+    }
+
 
 
 
